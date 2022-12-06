89871b3b081da20719f0a1dab13829e7d7eb7f6d
rbhatt199924 <rbhatt199924@gmail.com>
2022-12-06 20:56:03 +0530

    add linter and webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { mainModule } = require('process');
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname,'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:['style-loader', 'css-loader'],
+      }
+    ]
+  }
+}
